fdfcd1f5f2c7756d8b6e9d14e6ee151866de09aa
Asukulu Ndela <200056500@aston.ac.uk>
2024-08-11 01:29:25 +0100

    creating folders and do some changes

--- back_end/routes/stuff.js
+++ back_end/routes/stuff.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const auth = require('../middleware/auth');
 
 const router = express.Router();
 //const Item = require('../models/item');
@@ -7,18 +8,18 @@ const router = express.Router();
 const stuffCtrl = require('../controllers/stuff');
 
 // POST route
-router.post('/', stuffCtrl.createItem);
+router.post('/',auth,  stuffCtrl.createItem);
 
 //Get route for item ,Edit and update
-  router.get('/:id', stuffCtrl.getOneItem);
+  router.get('/:id',auth,  stuffCtrl.getOneItem);
   
   // GET route find
-  router.get('/', stuffCtrl.getAllItem);
+  router.get('/', auth, stuffCtrl.getAllItem);
 
   //Get route for delete
-  router.delete('/:id', stuffCtrl.deleteItem);
+  router.delete('/:id', auth, stuffCtrl.deleteItem);
   
   //Put route for update
-  router.put('/:id', stuffCtrl.modifyItem);
+  router.put('/:id', auth, stuffCtrl.modifyItem);
 
 module.exports = router;
